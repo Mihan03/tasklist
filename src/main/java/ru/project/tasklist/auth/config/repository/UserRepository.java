7519ea25c3f09402a14b8323af32c375ca9ba609
Mikhail Feoktistov <mihan0@mail.ru>
2021-09-16 22:48:38 +0300

    added UserDetails

--- src/main/java/ru/project/tasklist/auth/config/repository/UserRepository.java
+++ src/main/java/ru/project/tasklist/auth/config/repository/UserRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.project.tasklist.auth.config.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select case when count(u)> 0 then true else false end from User u where lower(u.email) = lower(:email)")
@@ -13,6 +15,13 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("select case when count(u)> 0 then true else false end from User u where lower(u.username) = lower(:username)")
     boolean existsByUsername(@Param("username") String username);
+
+    // используем обертку Optional - контейнер, который хранит значение или null - позволяет избежать ошибки NullPointerException
+    Optional<User> findByUsername(String username); // поиск по username
+
+    // используем обертку Optional - контейнер, который хранит значение или null - позволяет избежать ошибки NullPointerException
+    Optional<User> findByEmail(String email); // поиск по email
+
 
 
 }
